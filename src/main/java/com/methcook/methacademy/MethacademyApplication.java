0be5593f887387b98f4a8fcfa3f2df7c2f3f09b6
Hiratsuna <adhdmicromanagement@outlook.com>
2023-07-29 14:49:26 +0300

    bum bam

--- src/main/java/com/methcook/methacademy/MethacademyApplication.java
+++ src/main/java/com/methcook/methacademy/MethacademyApplication.java
@@ -2,8 +2,10 @@ package com.methcook.methacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MethacademyApplication {
 
 	public static void main(String[] args) {
