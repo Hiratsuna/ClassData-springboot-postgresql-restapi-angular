10abd1d80caabf1e12c7f34108a6a22ae10754a6
Hiratsuna <adhdmicromanagement@outlook.com>
2023-08-02 08:39:30 +0300

    DAI MUUU

--- src/main/java/com/methcook/methacademy/controller/StudentsController.java
+++ src/main/java/com/methcook/methacademy/controller/StudentsController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,8 +31,8 @@ public class StudentsController {
 	
 	
 	// get students
-	
 	@GetMapping("students/")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public List<Student> getAllStudents(){
 		return this.studentsRepository.findAll();
 	}
